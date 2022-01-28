bcc707fb28280627e56664e1c2c1f82e1567c81c
MessiasFilho <messiasfilho98@gmail.com>
2022-01-28 17:10:24 -0300

    Create DBexp.java

---
+++ src/BD/DBexp.java
@@ -0,0 +1,14 @@
+package BD;
+
+public class DBexp extends RuntimeException  {
+
+	private static final long serialVersionUID = 1L;
+
+	public DBexp (String msg) {
+		super (msg); 
+	}
+	
+	
+	
+	
+}
