8f5752b0ab8f0ee7ccf35261f545bce8eaa09b66
MessiasFilho <messiasfilho98@gmail.com>
2022-01-28 19:57:24 -0300

    Update Program.java

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,37 +1,54 @@
 package Aplication;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import BD.Conecxao;
+import Model.dao.DepartmentDao;
+import Model.dao.FactoryDao;
+import Model.dao.SellerDao;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		
 		Connection con =  null ; 
-		Statement st = null ; 
+		Statement pt = null ; 
 		ResultSet rs = null ; 
 		
-		try {
-			con = Conecxao.getConnection() ; 
-			st = con.createStatement();
-			rs =st.executeQuery("SELECT * FROM Seller"); 
-			
-			while (rs.next() ) {
-				System.out.println(rs.getString("Name"));
-			}	
 		
+	
+		
+				try {
+			
+			con = Conecxao.getConnection();
+			pt = con.createStatement(); 
+			
+			rs = pt.executeQuery("SELECT * FROM Department"); 
+			
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + " "+rs.getString("Name"));
+			}
+		
+
 
 		}catch (SQLException e ) {
 			e.printStackTrace();
 		
 		}finally {
-			
+			Conecxao.CloseSatement(pt);
+			Conecxao.CloseResultSet(rs); 
 			Conecxao.CloseConnection() ; 
+			
+			
+			
 		}
 		
 		
