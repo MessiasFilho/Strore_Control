00d588ac50774a0b0e1039eb275d706fc0c12713
MessiasFilho <messiasfilho98@gmail.com>
2022-01-28 19:57:00 -0300

    Create DepartmentJDBC.java

---
+++ src/Model/dao/JDBC/DepartmentJDBC.java
@@ -0,0 +1,40 @@
+package Model.dao.JDBC;
+
+import java.util.List;
+
+import Entityes.Department;
+import Model.dao.DepartmentDao;
+
+public class DepartmentJDBC implements DepartmentDao {
+
+	@Override
+	public void Insert(Department Obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department Obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DeleteById(Integer Id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department FindById(Integer Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> FindALL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
